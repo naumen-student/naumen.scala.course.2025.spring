20161cdb817cb2c48a6226a81409f8c29db00ce8
Frisko01 <dan.daniil-dan@yandex.ru>
2025-04-24 19:14:14 +0500

    homework_5 — Веретенников Даниил

--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -12,6 +12,14 @@ import scala.concurrent.duration.DurationInt
   Обязательно использовать функцию mapReduce.
  */
 object Task3 extends App {
+  def countWords(lines: Vector[String]): WordsCount = {
+    lines
+      .map(line => Count(line, line.length))
+    val res = mapReduce[String, WordsCount](lines)(line => WordsCount(line.split(" ")
+      .groupBy(identity).map(x => Count(x._1, x._2.length)).toSeq))
+    Await.result(res, 5.seconds)
+  }
+
   def mapReduce[A, B: Monoid](values: Vector[A])(func: A => B): Future[B] = {
     val numCores = Runtime.getRuntime.availableProcessors
     val groupSize = (1.0 * values.size / numCores).ceil.toInt
@@ -25,8 +33,13 @@ object Task3 extends App {
   case class Count(word: String, count: Int)
   case class WordsCount(count: Seq[Count])
   object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
+    implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount] {
+      override def empty: WordsCount = new WordsCount(count = Seq[Count]())
+      override def combine(x: WordsCount, y: WordsCount): WordsCount =
+        WordsCount((x.count ++ y.count)
+          .groupBy(_.word)
+          .map(x =>
+            Count(x._1, x._2.foldLeft(0)((cur, next) => cur + next.count))).toSeq)
+    }
   }
-
-  def countWords(lines: Vector[String]): WordsCount = ???
 }
