9f1f136f4ff64edfc78e7ed4c8293d4c17a72a5e
bemanelw <chernyugov04@mail.ru>
2025-04-25 14:04:40 +0300

    homework_5 Чернюгов Лев

--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -11,6 +11,13 @@ import scala.concurrent.duration.DurationInt
   Реализуйте функцию countWords, которая принимает список строк.
   Обязательно использовать функцию mapReduce.
  */
+import cats._
+import cats.implicits._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+
 object Task3 extends App {
   def mapReduce[A, B: Monoid](values: Vector[A])(func: A => B): Future[B] = {
     val numCores = Runtime.getRuntime.availableProcessors
@@ -25,8 +32,26 @@ object Task3 extends App {
   case class Count(word: String, count: Int)
   case class WordsCount(count: Seq[Count])
   object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
+    implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount] {
+      def empty: WordsCount = WordsCount(Seq.empty)
+      def combine(x: WordsCount, y: WordsCount): WordsCount = {
+        val combinedCounts = (x.count ++ y.count)
+          .groupBy(_.word)
+          .map { case (word, counts) =>
+            Count(word, counts.map(_.count).sum)
+          }
+          .toSeq
+        WordsCount(combinedCounts)
+      }
+    }
   }
 
-  def countWords(lines: Vector[String]): WordsCount = ???
+  def countWords(lines: Vector[String]): WordsCount = {
+    val words = lines.flatMap(_.split("\\s+"))
+
+    Await.result(
+      mapReduce(words)(word => WordsCount(Seq(Count(word, 1)))),
+      1.second
+    )
+  }
 }
