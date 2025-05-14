8977e2cda7a4c83c12efce4703686e9345bab06d
VlttVizner <biolavizner1212@icloud.com>
2025-05-14 20:11:28 +0500

    add homework_5

--- homeworks/homework_5/src/main/scala/Task4.scala
+++ homeworks/homework_5/src/main/scala/Task4.scala
@@ -1,36 +1,46 @@
 import scala.language.higherKinds
 import scala.util.{Failure, Success, Try}
 
-/*
-  Задание №4
-  Давайте реализуем свою монаду для обработки ошибок.
-  Нужно:
-  1) Реализовать функцию map в трейте MonadError
-  2) Написать инстанс MonadError для EIO
-  3) Реализовать функцию possibleError для обработки кода, который может вызывать ошибку
-  Примеры использования можно посмотреть в тестах.
-  Подсказка: На Either определён flatMap, его можно переиспользовать
- */
 object Task4 extends App {
   trait MonadError[F[_, _], E] {
     def pure[A](value: A): F[E, A]
+
     def flatMap[A, B](fa: F[E, A])(f: A => F[E, B]): F[E, B]
 
-    def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = ???
+    def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = flatMap(fa)(a => pure(f(a)))
 
-    def raiseError[A](fa: F[E, A])(error: => E):  F[E, A]
+    def raiseError[A](fa: F[E, A])(error: => E): F[E, A]
+
     def handleError[A](fa: F[E, A])(handle: E => A): F[E, A]
   }
 
   case class EIO[+E, +A](value: Either[E, A])
+
   object EIO {
     def apply[A](value: A): EIO[Nothing, A] = EIO[Nothing, A](Right(value))
 
     def error[E, A](error: E): EIO[E, A] = EIO[E, A](Left(error))
 
-    def possibleError[A](f: => A): EIO[Throwable, A] = ???
+    def possibleError[A](f: => A): EIO[Throwable, A] =
+      Try(f) match {
+        case Success(value) => EIO(value)
+        case Failure(err) => EIO.error(err)
+      }
 
-    implicit def monad[E]: MonadError[EIO, E] = ???
+    implicit def monad[E]: MonadError[EIO, E] = new MonadError[EIO, E] {
+      override def pure[A](value: A): EIO[E, A] = EIO(value)
+
+      override def flatMap[A, B](fa: EIO[E, A])(f: A => EIO[E, B]): EIO[E, B] =
+        EIO(fa.value.flatMap(a => f(a).value))
+
+      override def raiseError[A](fa: EIO[E, A])(error: => E): EIO[E, A] = EIO(Left(error))
+
+      override def handleError[A](fa: EIO[E, A])(handle: E => A): EIO[E, A] =
+        EIO(fa.value match {
+          case Left(e) => Right(handle(e))
+          case Right(v) => Right(v)
+        })
+    }
   }
 
   object EIOSyntax {
