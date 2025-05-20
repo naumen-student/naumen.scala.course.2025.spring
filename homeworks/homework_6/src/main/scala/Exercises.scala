13df95a1f2b83fa013b9925379dc8a30d3d6688c
Марина Юрьевна Апкаева <132090822+WilgardWolf@users.noreply.github.com>
2025-05-20 18:18:38 +0500

    hw-6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -2,6 +2,7 @@ import utils.ColorService.ColorService
 import utils.PictureGenerationService.PictureGenerationService
 import utils.Utils._
 import zio.{IO, Random, URIO, ZIO}
+import utils.{ColorService, PictureGenerationService}
 
 import java.awt.Color
 
@@ -12,7 +13,7 @@ object Exercises {
      * вернулся None, а в случае упеха Some
      */
     def task1(r: Int, g: Int, b: Int): URIO[ColorService, Option[Color]] =
-        ZIO.serviceWithZIO[ColorService](_.getColor(r, g, b))
+        ZIO.serviceWithZIO[ColorService](_.getColor(r, g, b)).option
 
 
     /**
@@ -23,6 +24,10 @@ object Exercises {
      */
     def task2(size: (Int, Int)): ZIO[PictureGenerationService, GenerationError, String] =
         ZIO.serviceWithZIO[PictureGenerationService](_.generatePicture(size))
+          .map(picture => picture.lines.map(
+              line => line.map(
+                  x => Integer.toUnsignedString(x.getRGB)
+              ).mkString(" ")).mkString("\n"))
 
 
     /**
@@ -38,8 +43,13 @@ object Exercises {
             colorServ <- ZIO.service[ColorService]
             pictureServ <- ZIO.service[PictureGenerationService]
             color <- colorServ.generateRandomColor()
+              .mapError(_ => new GenerationError("Не удалось создать цвет"))
+
             picture <- pictureServ.generatePicture(size)
+              .mapError(_ => new GenerationError("Ошибка генерации изображения"))
+
             filledPicture <- pictureServ.fillPicture(picture, color)
+              .mapError(_ => new GenerationError("Возникли проблемы при заливке изображения"))
         } yield filledPicture
 
     /**
@@ -47,5 +57,6 @@ object Exercises {
      */
     def task4(size: (Int, Int)): IO[GenerationError, Picture] =
         task3(size)
-
+          .provideSomeLayer[ColorService](PictureGenerationService.live)
+          .provideLayer(ColorService.live)
 }
