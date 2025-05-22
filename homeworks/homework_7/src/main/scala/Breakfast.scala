f7138e3dd4c26a103d615b5a69ee9b91a422efca
Ilya <74655944+JabaXNT@users.noreply.github.com>
2025-05-22 17:52:27 +0500

    homework_7 - Глухов Илья

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,13 +1,12 @@
 package ru.dru
 
-import zio.CanFail.canFailAmbiguous1
-import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.{Clock, Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.Clock.currentDateTime
 
 import java.time.LocalDateTime
 import scala.concurrent.TimeoutException
 
 case class SaladInfoTime(tomatoTime: Duration, cucumberTime: Duration)
-
 
 object Breakfast extends ZIOAppDefault {
 
@@ -32,10 +31,31 @@ object Breakfast extends ZIOAppDefault {
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = {
+    for {
+      now <- ZIO.succeed(LocalDateTime.now())
+      eggsFiber <- (ZIO.sleep(eggsFiringTime) *> ZIO.succeed(LocalDateTime.now())).fork
+      waterFiber <- (ZIO.sleep(waterBoilingTime) *> ZIO.succeed(LocalDateTime.now())).fork
+      saladFiber <- (for {
+        _ <- ZIO.sleep(saladInfoTime.cucumberTime)
+        _ <- ZIO.sleep(saladInfoTime.tomatoTime)
+        saladTime <- ZIO.succeed(LocalDateTime.now())
+      } yield saladTime).fork
 
+      waterTime <- waterFiber.join
+      teaFiber <- (ZIO.sleep(teaBrewingTime) *> ZIO.succeed(LocalDateTime.now())).fork
 
+      eggsTime <- eggsFiber.join
+      saladTime <- saladFiber.join
+      teaTime <- teaFiber.join
+      result = Map(
+        "eggs" -> eggsTime,
+        "water" -> waterTime,
+        "saladWithSourCream" -> saladTime,
+        "tea" -> teaTime
+      )
+    } yield result
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
-
 }
--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -4,18 +4,40 @@ import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
-
 /**
  * Необходимо реализовать функции readData и writeData, записывающие и читающие данные в/из файла соответственно.
  * В реализации следует применять безопасное использование ресурсов ZIO.acquireReleaseWith
  */
 
-
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith(
+      ZIO.attempt(new BufferedReader(new FileReader(filePath)))
+    )(reader => ZIO.succeed(reader.close())) { reader =>
+      ZIO.attempt {
+        val sb = new StringBuilder
+        var line: String = reader.readLine()
+        while (line != null) {
+          sb.append(line)
+          line = reader.readLine()
+          if (line != null) sb.append("\n")
+        }
+        sb.toString
+      }
+    }
+  }
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+    ZIO.acquireReleaseWith(
+      ZIO.attempt(new BufferedWriter(new FileWriter(filePath)))
+    )(writer => ZIO.succeed(writer.close())) { writer =>
+      ZIO.attempt {
+        writer.write(data)
+        writer.flush()
+      }
+    }.orDie
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
