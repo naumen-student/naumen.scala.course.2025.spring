3bdca9ce711be7e8ee3820609d1ab2faa1d81867
FireSiemens <130760584+FireSiemens@users.noreply.github.com>
2025-05-24 00:00:56 +0300

    homework_6+7

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,4 +1,5 @@
 import utils.ColorService.ColorService
+import utils.{ColorService, PictureGenerationService}
 import utils.PictureGenerationService.PictureGenerationService
 import utils.Utils._
 import zio.{IO, Random, URIO, ZIO}
@@ -12,7 +13,7 @@ object Exercises {
      * вернулся None, а в случае упеха Some
      */
     def task1(r: Int, g: Int, b: Int): URIO[ColorService, Option[Color]] =
-        ZIO.serviceWithZIO[ColorService](_.getColor(r, g, b))
+        ZIO.serviceWithZIO[ColorService](_.getColor(r, g, b).option)
 
 
     /**
@@ -23,6 +24,7 @@ object Exercises {
      */
     def task2(size: (Int, Int)): ZIO[PictureGenerationService, GenerationError, String] =
         ZIO.serviceWithZIO[PictureGenerationService](_.generatePicture(size))
+          .map(x => x.lines.map(y => y.map(color => Integer.toUnsignedString(color.getRGB)).mkString(" ")).mkString("\n"))
 
 
     /**
@@ -37,15 +39,14 @@ object Exercises {
         for {
             colorServ <- ZIO.service[ColorService]
             pictureServ <- ZIO.service[PictureGenerationService]
-            color <- colorServ.generateRandomColor()
-            picture <- pictureServ.generatePicture(size)
-            filledPicture <- pictureServ.fillPicture(picture, color)
+            color <- colorServ.generateRandomColor().mapError(_ => new GenerationError("Не удалось создать цвет"))
+            picture <- pictureServ.generatePicture(size).mapError(_ => new GenerationError("Ошибка генерации изображения"))
+            filledPicture <- pictureServ.fillPicture(picture, color).mapError(_ => new GenerationError("Возникли проблемы при заливке изображения"))
         } yield filledPicture
 
     /**
      * Необходимо предоставить объекту ZIO все необходимые зависимости
      */
     def task4(size: (Int, Int)): IO[GenerationError, Picture] =
-        task3(size)
-
+        task3(size).provideSomeLayer(PictureGenerationService.live).provideLayer(ColorService.live)
 }
