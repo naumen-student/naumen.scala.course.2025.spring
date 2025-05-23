599573910777fb92d88d264c1286cb65df1bcf39
Nikita <34120307+ColdDoshirak@users.noreply.github.com>
2025-05-23 13:25:56 +0500

    Add files via upload

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,7 +1,7 @@
 package ru.dru
 
 import zio.CanFail.canFailAmbiguous1
-import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.{Clock, Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
 
 import java.time.LocalDateTime
 import scala.concurrent.TimeoutException
@@ -32,9 +32,40 @@ object Breakfast extends ZIOAppDefault {
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
-
-
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = {
+    
+    for {
+      startTime <- Clock.currentDateTime
+      
+      // Запускаем параллельно: яичницу, воду и салат
+      eggsFiber <- (ZIO.sleep(eggsFiringTime) *> Clock.currentDateTime.map(_.toLocalDateTime)).fork
+      waterFiber <- (ZIO.sleep(waterBoilingTime) *> Clock.currentDateTime.map(_.toLocalDateTime)).fork
+      
+      // Салат готовится последовательно: огурцы -> помидоры -> сметана
+      saladFiber <- (for {
+        _ <- ZIO.sleep(saladInfoTime.cucumberTime) // режем огурцы
+        _ <- ZIO.sleep(saladInfoTime.tomatoTime)   // режем помидоры
+        // добавляем сметану (мгновенно)
+        finishTime <- Clock.currentDateTime.map(_.toLocalDateTime)
+      } yield finishTime).fork
+      
+      // Ждем когда закипит вода
+      waterTime <- waterFiber.join
+      
+      // После кипячения воды завариваем чай
+      teaTime <- ZIO.sleep(teaBrewingTime) *> Clock.currentDateTime.map(_.toLocalDateTime)
+      
+      // Ждем готовности яичницы и салата
+      eggsTime <- eggsFiber.join
+      saladTime <- saladFiber.join
+      
+    } yield Map(
+      "eggs" -> eggsTime,
+      "water" -> waterTime,
+      "saladWithSourCream" -> saladTime,
+      "tea" -> teaTime
+    )
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
 
