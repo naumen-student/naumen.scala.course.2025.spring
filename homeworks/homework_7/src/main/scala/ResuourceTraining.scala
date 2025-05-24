c4507a7ff0ef64b184c6bbff8839dc7bbae5355b
ivan <qwuipss@icloud.com>
2025-05-24 21:00:32 +0500

    task 2

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -13,9 +13,30 @@ import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith(ZIO.attempt(new BufferedReader(new FileReader(filePath))))(reader =>
+      ZIO.attempt(reader.close()).orDie
+    ) { r =>
+      ZIO.attempt {
+        val sb = new StringBuilder
+        var str = r.readLine()
+        while (str != null) {
+          sb.append(str)
+          str = r.readLine()
+        }
+        sb.toString()
+      }.refineToOrDie[Throwable]
+    }
+  }
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  def writeData(filePath: String, data: String): ZIO[Any, Throwable, Unit] = {
+    ZIO.acquireReleaseWith(ZIO.attempt(new BufferedWriter(new FileWriter(filePath, false))))(writer => ZIO.attempt(writer.close()).orDie
+    ) {
+      w => ZIO.attempt(w.write(data))
+        .flatMap(_ => ZIO.attempt(w.flush()))
+        .refineToOrDie[Throwable]
+    }
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
